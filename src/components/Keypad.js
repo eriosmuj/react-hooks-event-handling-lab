8ac0b852b9bcaf47c112b672febb4e11d7aff4ca
Elizabeth Rios-Mujica <elizabeth.rmujica@gmail.com>
2022-05-17 19:47:19 -0400

    commit tree 848622b92c3706ea14bac0e17ff5c4d42c7c869d

--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,1 +1,14 @@
 // Code Keypad Component Here
+
+
+function Keypad() {
+  return (
+    <div>
+        <form>
+            <input type="password" onChange={event => console.log('Entering password...')} />
+        </form>
+    </div>
+  );
+}
+
+export default Keypad;
